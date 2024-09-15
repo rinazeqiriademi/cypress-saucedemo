3a495a1608c4493ce28dd6c7bd4ceb412e33c1d2
Rina Ademi <rina.zeqiri1@gmail.com>
2024-09-15 22:26:01 +0200

    Add new test for login page

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -83,4 +83,28 @@ describe('Login Page Tests', () => {
       cy.get('[data-test="error"]').should('be.visible').and('contain', 'Epic sadface: Username and password do not match any user in this service');
     });
 
+    it('should be case-sensitive for usernames', () => {
+      const uppercaseUsername = validUsernames[0].toUpperCase();
+      login(uppercaseUsername, validPassword);
+      cy.get('[data-test="error"]').should('be.visible').and('contain', 'Epic sadface: Username and password do not match any user in this service');
+    });
+
+    it('should be case-sensitive for passwords', () => {
+      const uppercasePassword = validPassword.toUpperCase();
+      login(validUsernames[0], uppercasePassword);
+      cy.get('[data-test="error"]').should('be.visible').and('contain', 'Epic sadface: Username and password do not match any user in this service');
+    });
+
+    it('should trim whitespace from username input', () => {
+      login(`  ${validUsernames[0]}  `, validPassword);
+       // Check that user is successfully logged in and can see the inventory page
+      cy.url().should('include', '/inventory.html');
+      logout();
+    });
+
+    it('should not trim whitespace from password input', () => {
+      login(validUsernames[0], ` ${validPassword} `);
+      cy.get('[data-test="error"]').should('be.visible').and('contain', 'Epic sadface: Username and password do not match any user in this service');
+    });
+
 })
