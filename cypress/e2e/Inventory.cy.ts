44cf72ed2174c896e54afb94f1f1c6fd633c96fe
Rina Ademi <rina.zeqiri1@gmail.com>
2024-09-16 22:05:24 +0200

    Add tests for inventory page

---
+++ cypress/e2e/Inventory.cy.ts
@@ -0,0 +1,82 @@
+describe('Inventory Page Tests', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:3000/');
+        cy.get('[data-test="username"]').type('standard_user');
+        cy.get('[data-test="password"]').type('secret_sauce');
+        cy.get('[data-test="login-button"]').click();
+
+        cy.url().should('include', '/inventory.html');
+    });
+
+    it('displays the correct page title', () => {
+        cy.get('.title').should('have.text', 'Products');
+    });
+
+    it('allows sorting products by name (A to Z)', () => {
+        cy.get('[data-test="product-sort-container"]').select('az');
+        cy.get('.inventory_item_name').then($items => {
+            const itemNames = $items.map((i, el) => Cypress.$(el).text()).get();
+            const sortedNames = [...itemNames].sort();
+            expect(itemNames).to.deep.equal(sortedNames);
+        });
+    });
+
+    it('allows adding a single product to the cart', () => {
+        cy.get('.inventory_item').first().find('.btn_inventory').click();
+        cy.get('.shopping_cart_badge').should('have.text', '1');
+    });
+
+    it('allows adding multiple products to the cart', () => {
+        cy.get('.inventory_item').eq(0).find('.btn_inventory').click();
+        cy.get('.inventory_item').eq(1).find('.btn_inventory').click();
+        cy.get('.inventory_item').eq(2).find('.btn_inventory').click();
+        cy.get('.inventory_item').eq(3).find('.btn_inventory').click();
+        cy.get('.inventory_item').eq(4).find('.btn_inventory').click();
+        cy.get('.inventory_item').eq(5).find('.btn_inventory').click();
+        cy.get('.shopping_cart_badge').should('have.text', '6');
+    });
+
+
+    it('allows removing a product from the cart', () => {
+        cy.get('.inventory_item').first().find('.btn_inventory').click();
+        cy.get('.shopping_cart_badge').should('have.text', '1');
+        cy.get('.inventory_item').first().find('.btn_inventory').click();
+        cy.get('.shopping_cart_badge').should('not.exist');
+    })
+
+    it('updates button text when adding/removing items', () => {
+        cy.get('.inventory_item').first().within(() => {
+            cy.get('.btn_inventory').should('have.text', 'Add to cart');
+            cy.get('.btn_inventory').click();
+            cy.get('.btn_inventory').should('have.text', 'Remove');
+            cy.get('.btn_inventory').click();
+            cy.get('.btn_inventory').should('have.text', 'Add to cart');
+        })
+    });
+
+    it('navigates to product details page when clicking on product name', () => {
+        cy.get('.inventory_item_name').first().click();
+        cy.url().should('include', '/inventory-item.html');
+    });
+
+    it('navigates to product details page when clicking on product image', () => {
+        cy.get('.inventory_item_img').first().click();
+        cy.url().should('include', '/inventory-item.html');
+    });
+
+    it('opens the shopping cart when clicking the cart icon', () => {
+        cy.get('.shopping_cart_link').click();
+        cy.url().should('include', '/cart.html');
+    });
+
+    it('allows accessing the burger menu', () => {
+        cy.get('#react-burger-menu-btn').click();
+        cy.get('.bm-menu-wrap').should('be.visible');
+    });
+
+    it('displays footer content', () => {
+        cy.get('.footer').should('be.visible');
+        cy.get('.social').should('be.visible');
+    });
+
+});
--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -5,20 +5,20 @@ describe('Login Page Tests', () => {
         'performance_glitch_user',
         'error_user',
         'visual_user',
-      ];
+    ];
 
-     const validPassword = 'secret_sauce';
-     const invalidPassword = 'wrong_password';
-     const invalidUsername = 'invalid_user';
+    const validPassword = 'secret_sauce';
+    const invalidPassword = 'wrong_password';
+    const invalidUsername = 'invalid_user';
 
     beforeEach(() => {
         cy.visit('http://localhost:3000/')
     })
 
     const login = (username: string, password: string) => {
-          cy.get('[data-test="username"]').type(username);
-          cy.get('[data-test="password"]').type(password);
-          cy.get('[data-test="login-button"]').click();
+        cy.get('[data-test="username"]').type(username);
+        cy.get('[data-test="password"]').type(password);
+        cy.get('[data-test="login-button"]').click();
     };
 
     const logout = () => {
@@ -28,83 +28,83 @@ describe('Login Page Tests', () => {
 
     validUsernames.forEach((username) => {
         it(`should allow valid user ${username} to login with correct password`, () => {
-          login(username, validPassword);
-    
-          // Check that user is successfully logged in and can see the inventory page
-          cy.url().should('include', '/inventory.html');
+            login(username, validPassword);
 
-          logout();
+            // Check that user is successfully logged in and can see the inventory page
+            cy.url().should('include', '/inventory.html');
+
+            logout();
         });
-      });
+    });
 
     it('should display error message for invalid username', () => {
-      login(invalidUsername, validPassword);
-      cy.get('[data-test="error"]').should('be.visible').and('contain', 'Epic sadface: Username and password do not match any user in this service');
+        login(invalidUsername, validPassword);
+        cy.get('[data-test="error"]').should('be.visible').and('contain', 'Epic sadface: Username and password do not match any user in this service');
     });
 
     it('should display error message for invalid password', () => {
-      login(validUsernames[0], invalidPassword);
-      cy.get('[data-test="error"]').should('be.visible').and('contain', 'Epic sadface: Username and password do not match any user in this service');
+        login(validUsernames[0], invalidPassword);
+        cy.get('[data-test="error"]').should('be.visible').and('contain', 'Epic sadface: Username and password do not match any user in this service');
     });
 
     it('should display error message for locked out user', () => {
-      login('locked_out_user', validPassword);
-      cy.get('[data-test="error"]').should('be.visible').and('contain', 'Epic sadface: Sorry, this user has been locked out.');
+        login('locked_out_user', validPassword);
+        cy.get('[data-test="error"]').should('be.visible').and('contain', 'Epic sadface: Sorry, this user has been locked out.');
     });
 
     it('should display error message for empty username', () => {
-      cy.get('[data-test="username"]');
-      cy.get('[data-test="password"]').type(validPassword);
-      cy.get('[data-test="login-button"]').click();
-      cy.get('[data-test="error"]').should('be.visible').and('contain', 'Epic sadface: Username is required');
+        cy.get('[data-test="username"]');
+        cy.get('[data-test="password"]').type(validPassword);
+        cy.get('[data-test="login-button"]').click();
+        cy.get('[data-test="error"]').should('be.visible').and('contain', 'Epic sadface: Username is required');
     });
 
     it('should display error message for empty password', () => {
-      cy.get('[data-test="username"]').type(validUsernames.at(0))
-      cy.get('[data-test="password"]');
-      cy.get('[data-test="login-button"]').click();
-      cy.get('[data-test="error"]').should('be.visible').and('contain', 'Epic sadface: Password is required');
+        cy.get('[data-test="username"]').type(validUsernames.at(0))
+        cy.get('[data-test="password"]');
+        cy.get('[data-test="login-button"]').click();
+        cy.get('[data-test="error"]').should('be.visible').and('contain', 'Epic sadface: Password is required');
     });
 
     it('should display error message for empty username and password', () => {
-      cy.get('[data-test="username"]');
-      cy.get('[data-test="password"]');
-      cy.get('[data-test="login-button"]').click();
-      cy.get('[data-test="error"]').should('be.visible').and('contain', 'Epic sadface: Username is required');
+        cy.get('[data-test="username"]');
+        cy.get('[data-test="password"]');
+        cy.get('[data-test="login-button"]').click();
+        cy.get('[data-test="error"]').should('be.visible').and('contain', 'Epic sadface: Username is required');
     });
 
-     it('should display error message for invalid username and password', () => {
-      login(invalidUsername, invalidPassword);
-      cy.get('[data-test="error"]').should('be.visible').and('contain', 'Epic sadface: Username and password do not match any user in this service');
+    it('should display error message for invalid username and password', () => {
+        login(invalidUsername, invalidPassword);
+        cy.get('[data-test="error"]').should('be.visible').and('contain', 'Epic sadface: Username and password do not match any user in this service');
     });
 
     it('should display error message that username and password do not match for locked out user and invalid password', () => {
-      login('locked_out_user', invalidPassword);
-      cy.get('[data-test="error"]').should('be.visible').and('contain', 'Epic sadface: Username and password do not match any user in this service');
+        login('locked_out_user', invalidPassword);
+        cy.get('[data-test="error"]').should('be.visible').and('contain', 'Epic sadface: Username and password do not match any user in this service');
     });
 
     it('should be case-sensitive for usernames', () => {
-      const uppercaseUsername = validUsernames[0].toUpperCase();
-      login(uppercaseUsername, validPassword);
-      cy.get('[data-test="error"]').should('be.visible').and('contain', 'Epic sadface: Username and password do not match any user in this service');
+        const uppercaseUsername = validUsernames[0].toUpperCase();
+        login(uppercaseUsername, validPassword);
+        cy.get('[data-test="error"]').should('be.visible').and('contain', 'Epic sadface: Username and password do not match any user in this service');
     });
 
     it('should be case-sensitive for passwords', () => {
-      const uppercasePassword = validPassword.toUpperCase();
-      login(validUsernames[0], uppercasePassword);
-      cy.get('[data-test="error"]').should('be.visible').and('contain', 'Epic sadface: Username and password do not match any user in this service');
+        const uppercasePassword = validPassword.toUpperCase();
+        login(validUsernames[0], uppercasePassword);
+        cy.get('[data-test="error"]').should('be.visible').and('contain', 'Epic sadface: Username and password do not match any user in this service');
     });
 
     it('should trim whitespace from username input', () => {
-      login(`  ${validUsernames[0]}  `, validPassword);
-       // Check that user is successfully logged in and can see the inventory page
-      cy.url().should('include', '/inventory.html');
-      logout();
+        login(`  ${validUsernames[0]}  `, validPassword);
+        // Check that user is successfully logged in and can see the inventory page
+        cy.url().should('include', '/inventory.html');
+        logout();
     });
 
     it('should not trim whitespace from password input', () => {
-      login(validUsernames[0], ` ${validPassword} `);
-      cy.get('[data-test="error"]').should('be.visible').and('contain', 'Epic sadface: Username and password do not match any user in this service');
+        login(validUsernames[0], ` ${validPassword} `);
+        cy.get('[data-test="error"]').should('be.visible').and('contain', 'Epic sadface: Username and password do not match any user in this service');
     });
 
 })
